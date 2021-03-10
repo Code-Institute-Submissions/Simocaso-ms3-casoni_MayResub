9fc00601187b490fb57a01a255200200c2d84213
Simocaso <simonecasoni97@gmail.com>
2021-03-10 01:55:41 +0000

    installed passlib package

--- user/models.py
+++ user/models.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify, request
+from passlib.hash import pdkdf2_sha256
 import uuid
 
 class User:
 
     def signup(self):
 
+        # this function create the user object when signin in
         user = {
             "_id": uuid.uuid4().hex,
             "name": request.form.get('name'),
