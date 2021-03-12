e3fbeb9858fc336382d749c55ad130215b87226e
Simocaso <simonecasoni97@gmail.com>
2021-03-12 00:00:51 +0000

    added js function to redirect user after signup

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -11,11 +11,11 @@ $("form[name=signup_form]").submit(function(e) {
     data: data,
     dataType: "json",
     success: function(resp) {
-      console.log(resp);
+    // if succesful, user will be redirect
+        window.location.href="/dashboard/"
     },
     error: function(resp) {
-      console.log(resp);
-// error message shown above sign up button, error refer to error in models.py 
+    // error message shown above sign up button, error refer to error in models.py 
       $error.text(resp.responseJSON.error).removeClass("error--hidden")
     }
   });
