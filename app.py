f98e5adb73e63049756917c322fa985fe0bb97fe
Simocaso <simonecasoni97@gmail.com>
2021-03-13 00:18:02 +0000

    adjusted routes

--- app.py
+++ app.py
@@ -1,15 +1,47 @@
-from flask import (
-    Flask, render_template, url_for,
-    redirect, request, session)
-from config import app, mongo
 import os
-from user import routes
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-
+from config import app, mongo
+from flask import (
+    Flask, render_template, url_for, flash,
+    redirect, request, session)
+from functools import wraps
 
 if os.path.exists("env.py"):
     import env
+
+# Decorators; see here for more details: https://towardsdatascience.com/a-primer-on-args-kwargs-decorators-for-data-scientists-bb8129e756a7, https://stackoverflow.com/questions/49376371/python-decorators-args-and-kwargs
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect("/")
+    return wrap      
+
+
+# routes
+@app.route("/")
+@app.route("/home")
+def home():
+    details = mongo.db.details.find()
+    return render_template("pages/home.html", details=details)
+
+    
+@app.route("/dashboard/")
+@login_required
+def dashboard():
+    return render_template("pages/dashboard.html")
+
+
+@app.route("/contact/")
+def contact():
+    return render_template("pages/contact.html")
+
+
+# This route handles 404 errors
+@app.errorhandler(404)
+def invalid_route(e):
+    return render_template('pages/404.html',  title="Page Not Found")
 
 
 if __name__ == "__main__":
--- user/routes.py
+++ user/routes.py
@@ -1,30 +1,7 @@
-from flask import render_template, url_for, flash, redirect, request, session
-from bson.objectid import ObjectId
 from config import app, mongo
+from flask import Flask
 # from user.models (folder/file) import User (class in the file)
 from user.models import User
-# from app (file/module) import app (instance of flask)
-from app import app
-from functools import wraps
-
-
-# Decorators; see here for more details: https://towardsdatascience.com/a-primer-on-args-kwargs-decorators-for-data-scientists-bb8129e756a7, https://stackoverflow.com/questions/49376371/python-decorators-args-and-kwargs
-def login_required(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'logged in' in session:
-            return f(*args, **kwargs)
-        else:
-            return redirect("/")
-    return wrap      
-
-
-# routes
-@app.route("/")
-@app.route("/home")
-def home():
-    details = mongo.db.details.find()
-    return render_template("pages/home.html", details=details)
 
 
 @app.route("/signup", methods=["POST"])
@@ -32,28 +9,11 @@ def signup():
     return User().signup() 
 
 
-@app.route("/login", methods=["POST"])
-def login():
-    return User().login() 
-
-
 @app.route("/signout")
 def signout():
     return User().signout()
 
 
-@app.route("/dashboard/")
-@login_required
-def dashboard():
-    return render_template("pages/dashboard.html")
-
-
-@app.route("/contact/")
-def contact():
-    return render_template("pages/contact.html")
-
-
-# This route handles 404 errors
-@app.errorhandler(404)
-def invalid_route(e):
-    return render_template('pages/404.html',  title="Page Not Found")
+@app.route("/login", methods=["POST"])
+def login():
+    return User().login() 
