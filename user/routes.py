587a19fd746963352ef21b07ea18c5e7767ac59a
Simocaso <simonecasoni97@gmail.com>
2021-03-13 11:35:59 +0000

    fixing routes / and '

--- user/routes.py
+++ user/routes.py
@@ -4,16 +4,16 @@ from flask import Flask
 from user.models import User
 
 
-@app.route("/signup", methods=["POST"])
+@app.route('/user/signup', methods=['POST'])
 def signup():
     return User().signup() 
 
 
-@app.route("/signout")
+@app.route('/user/signout')
 def signout():
     return User().signout()
 
 
-@app.route("/login", methods=["POST"])
+@app.route("/user/login", methods=["POST"])
 def login():
     return User().login() 
