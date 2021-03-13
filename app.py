0b69510b7e4ae3114b4f74d3de36e15e9868ffb4
Simocaso <simonecasoni97@gmail.com>
2021-03-13 00:29:05 +0000

    added js script for login

--- app.py
+++ app.py
@@ -5,19 +5,18 @@ from flask import (
     redirect, request, session)
 from functools import wraps
 
-if os.path.exists("env.py"):
+if os.path.exists('env.py'):
     import env
 
 # Decorators; see here for more details: https://towardsdatascience.com/a-primer-on-args-kwargs-decorators-for-data-scientists-bb8129e756a7, https://stackoverflow.com/questions/49376371/python-decorators-args-and-kwargs
 def login_required(f):
     @wraps(f)
     def wrap(*args, **kwargs):
-        if 'logged in' in session:
+        if 'logged_in' in session:
             return f(*args, **kwargs)
         else:
-            return redirect("/")
-    return wrap      
-
+            return redirect('/')     
+    return wrap
 
 # routes
 @app.route("/")
@@ -26,7 +25,7 @@ def home():
     details = mongo.db.details.find()
     return render_template("pages/home.html", details=details)
 
-    
+
 @app.route("/dashboard/")
 @login_required
 def dashboard():
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,26 +1,42 @@
-$("#signup_form").submit(function(e) {
-// retrieve form from its id/name https://stackoverflow.com/questions/10398783/jquery-serialize-form-and-other-parameters
+$("form[name=signup_form").submit(function(e) {
+    var $form = $(this);
+    var $error = $form.find(".error");
+    var data = $form.serialize();
 
-  var $form = $(this);
-  var $error = $form.find("#error");
-  var data = $form.serialize();
-  
-//  ajax call
-// this function will redirect the user to the dashboard once signed up
-  $.ajax({
-    url: "/signup",
-    type: "POST",
-    data: data,
-    dataType: "json",
-    success: function(resp) {
-        window.location.href = "/dashboard/";
-        // console.log(resp)
-    },
-    error: function(resp) {
-        $error.text(resp.responseJSON.error).removeClass("error--hidden");
-        // console.log(resp)
-    }
-  });
+    $.ajax({
+        url: "/signup",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: function(resp) {
+            window.location.href = "/dashboard/";
+        },
+        error: function(resp) {
+            $error.text(resp.responseJSON.error).removeClass("error--hidden");
+        }
+    });
 
-  e.preventDefault();
+    e.preventDefault();
 });
+
+$("form[name=login_form").submit(function(e) {
+    var $form = $(this);
+    var $error = $form.find(".error");
+    var data = $form.serialize();
+
+    $.ajax({
+        url: "/login",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: function(resp) {
+            window.location.href = "/dashboard/";
+        },
+        error: function(resp) {
+            $error.text(resp.responseJSON.error).removeClass("error--hidden");
+        }
+    });
+
+    e.preventDefault();
+});
+
