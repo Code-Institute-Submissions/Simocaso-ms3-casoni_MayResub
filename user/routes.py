f6b6f1db6c084e18a1be943bb784cefe29001c49
Simocaso <simonecasoni97@gmail.com>
2021-03-14 13:31:39 +0000

    finishing marked route

--- user/routes.py
+++ user/routes.py
@@ -1,5 +1,5 @@
 from config import app, mongo
-from flask import Flask
+from flask import Flask, redirect
 # , request, url_for
 # from user.models (folder/file) import User (class in the file)
 from user.models import User, Task
@@ -23,3 +23,8 @@ def login():
 @app.route("/user/addTask", methods=['POST'])
 def addTask():
     return Task().addTask()
+
+
+@app.route("/marked/<oid>")
+def marked(oid):
+    return redirect(url_for('pages/dashboard.html'))
