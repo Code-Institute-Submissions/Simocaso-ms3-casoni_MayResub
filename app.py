91d52acaf8848bc44e9287fac97b93b39fd6c933
Simocaso <simonecasoni97@gmail.com>
2021-03-15 11:49:44 +0000

    set debug to false

--- app.py
+++ app.py
@@ -51,6 +51,6 @@ def invalid_route(e):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
 
 # set to false before DEPLOYMENT!
